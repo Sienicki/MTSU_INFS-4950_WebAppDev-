40e86e3f87413b1bea6eefd9149a4ef7b0249446
Justin Sienicki <justinsienicki@att.net>
2021-02-10 14:06:17 -0600

    First Commit

---
+++ Cho2FutureValue/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cho2FutureValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Mary";
+            ViewBag.FV = "99999.99";
+
+
+            return View();
+        }
+    }
+}
