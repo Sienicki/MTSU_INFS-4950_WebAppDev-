3dc789b3dc0a525f97709bddcc7f532a824226f1
Justin Sienicki <justinsienicki@att.net>
2021-03-20 14:58:01 -0500

    Added Contact controller

---
+++ Ch04Contact_Manager/Controllers/ContactController.cs
@@ -0,0 +1,76 @@
+﻿using Ch04ContactManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ch04ContactManager.Controllers
+{
+    public class ContactController : Controller
+    {
+        private ContactContext context { get; set; }
+        public ContactController(ContactContext ctx)
+        {
+            context = ctx;
+        }
+        //Get Add
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            ViewBag.Categories = context.Categories.OrderBy(c => c.Name).ToList();
+            var model = new Contact();
+            return View("AddEdit", model);
+        }
+        //Get Edit
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            ViewBag.Categories = context.Categories.OrderBy(c => c.Name).ToList();
+            var model = context.Contacts.Find(id);
+            return View("AddEdit", model);
+        }
+        //Post Save
+        [HttpPost]
+        public IActionResult Save(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                if (contact.ContactID == 0)
+                {
+                    contact.DateAdded = DateTime.Now;
+                    context.Contacts.Add(contact);
+                }
+                else
+                {
+                    context.Contacts.Update(contact);
+                }
+                context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Action = (contact.ContactID == 0) ? "Add" : "Edit";
+                ViewBag.Action = context.Categories.OrderBy(c => c.Name).ToList();
+                return View("AddEdit", contact);
+            }
+        }
+        //Get delete
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var model = context.Categories.Find(id);
+            return View("Delete", model);
+        }
+        //Post Delete
+        [HttpPost]
+        public IActionResult Delete(Contact contact)
+        {
+            context.Contacts.Remove(contact);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
